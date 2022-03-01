efee3530bc6fdd80ed290cca31ed67810ec3f095
Zifeng Tang <tangzifeng37@yeah.net>
2022-03-01 01:29:03 +0000

    update: dynamic threshold

--- NLP_CW/NLP2077.py
+++ NLP_CW/NLP2077.py
@@ -7,13 +7,10 @@ Original file is located at
     https://colab.research.google.com/drive/1HrO9_fUvOaDklVloYwmGMLWRCGZHm5Ww
 """
 
-
-
-
 import os
 from dpm_preprocessing import DPMProprocessed
 import torch
-#from transformers import RobertaForSequenceClassification, RobertaTokenizer, Trainer, TrainingArguments, RobertaConfig
+# from transformers import RobertaForSequenceClassification, RobertaTokenizer, Trainer, TrainingArguments, RobertaConfig
 from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -22,9 +19,9 @@ from sklearn.metrics import classification_report
 import pandas as pd
 from sklearn.metrics import f1_score
 
-device = torch.device("cuda"  if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#dependencies
+# dependencies
 # ! python -m pip install nltk
 # ! python -m pip install wordcloud
 # ! python -m pip install Unidecode
@@ -35,33 +32,31 @@ os.system("python -m pip install wordcloud")
 os.system("python -m pip install Unidecode")
 os.system("python -m pip install beautifulsoup4")
 
-
-
 model_name = "microsoft/deberta-v2-xlarge"
-assert model_name in ['roberta-base', 'bert-base-uncased', 'google/electra-small-discriminator', "microsoft/deberta-v2-xlarge"]
+assert model_name in ['roberta-base', 'bert-base-uncased', 'google/electra-small-discriminator',
+                      "microsoft/deberta-v2-xlarge"]
 
 model_path = f'./models/pcl_{model_name}_finetuned/model/'
 tokenizer_path = f'./models/pcl_{model_name}_finetuned/tokenizer/'
 MAX_SEQ_LEN = 256
 
-WORKING_ENV = 'SERVER' # Can be JONAS, SERVER
+WORKING_ENV = 'SERVER'  #  Can be JONAS, SERVER
 assert WORKING_ENV in ['JONAS', 'SERVER']
 
 if WORKING_ENV == 'SERVER':
     temp_model_path = f'/hy-tmp/pcl/{model_name}/'
-if WORKING_ENV == 'JONAS': 
+if WORKING_ENV == 'JONAS':
     temp_model_path = f'./experiment/pcl/{model_name}/'
+
 
 class PCLDataset(torch.utils.data.Dataset):
 
     def __init__(self, tokenizer, input_set):
-
         self.tokenizer = tokenizer
         self.texts = list(input_set['text'])
         self.labels = list(input_set['label'])
-        
-    def collate_fn(self, batch):
 
+    def collate_fn(self, batch):
         texts = []
         labels = []
 
@@ -70,49 +65,48 @@ class PCLDataset(torch.utils.data.Dataset):
             labels.append(b['label'])
 
         encodings = self.tokenizer(texts, return_tensors='pt', padding=True, truncation=True, max_length=MAX_SEQ_LEN)
-        encodings['labels'] =  torch.tensor(labels)
+        encodings['labels'] = torch.tensor(labels)
         return encodings
-    
+
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, idx):
-       
         item = {'text': self.texts[idx],
                 'label': self.labels[idx]}
         return item
 
+
 config = AutoConfig.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name , config = config).to(device)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config).to(device)
 
 dpm_pp = DPMProprocessed('.', 'task4_test.tsv')
 
-
-df_type = 'BACKTRANS' # Can be UNBALANCED, BACKTRANS, OVERSAMPLING
+df_type = 'BACKTRANS'  #  Can be UNBALANCED, BACKTRANS, OVERSAMPLING
 assert df_type in ['UNBALANCED', 'BACKTRANS', 'OVERSAMPLING']
 
 if df_type == 'UNBALANCED':
     train_df_path = 'traindf.pickle'
     val_df_path = 'valdf.pickle'
-if df_type == 'BACKTRANS': 
+if df_type == 'BACKTRANS':
     train_df_path = 'traindf_backtrans.pickle'
     val_df_path = 'valdf_backtrans.pickle'
 
-
 if not os.path.isfile(train_df_path) or not os.path.isfile(val_df_path):
-  train_df, val_df = dpm_pp.get_unbalanced_split(0.1)
-  train_df.to_pickle('traindf.pickle')
-  val_df.to_pickle('valdf.pickle')
+    train_df, val_df = dpm_pp.get_unbalanced_split(0.1)
+    train_df.to_pickle('traindf.pickle')
+    val_df.to_pickle('valdf.pickle')
 else:
-  train_df = pd.read_pickle(train_df_path)
-  val_df = pd.read_pickle(val_df_path)
+    train_df = pd.read_pickle(train_df_path)
+    val_df = pd.read_pickle(val_df_path)
 
-print("Training set length: ",len(train_df))
-print("Validation set length: ",len(val_df))
+print("Training set length: ", len(train_df))
+print("Validation set length: ", len(val_df))
 
 train_dataset = PCLDataset(tokenizer, train_df)
 eval_dataset = PCLDataset(tokenizer, val_df)
+
 
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
@@ -126,34 +120,40 @@ class CustomTrainer(Trainer):
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return ((loss, outputs) if return_outputs else loss)
 
+
 validation_loader = DataLoader(eval_dataset)
+
+
 def compute_metric_eval(arg):
     logits, labels_gold = arg[0], arg[1]
-    labels_pred = np.argmax(logits, axis = 1)
-    return {'f1_macro' :f1_score(labels_gold, labels_pred, average='macro'), 
-            'pcl_f1': classification_report(labels_gold, labels_pred, target_names=["Not PCL","PCL"], output_dict= True)['PCL']['f1-score']} #more metrics can be added
+    labels_pred = np.argmax(logits, axis=1)
+    return {'f1_macro': f1_score(labels_gold, labels_pred, average='macro'),
+            'pcl_f1':
+                classification_report(labels_gold, labels_pred, target_names=["Not PCL", "PCL"], output_dict=True)[
+                    'PCL']['f1-score']}  # more metrics can be added
+
 
 training_args = TrainingArguments(
-        output_dir=temp_model_path,
-        learning_rate = 1e-6,
-        logging_steps= 100,
-        eval_steps = 500,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size = 4,
-        num_train_epochs = 3,
-        evaluation_strategy= "steps",
-        load_best_model_at_end=True,
-        metric_for_best_model='pcl_f1'
-        )
+    output_dir=temp_model_path,
+    learning_rate=1e-6,
+    logging_steps=100,
+    eval_steps=500,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    num_train_epochs=3,
+    evaluation_strategy="steps",
+    load_best_model_at_end=True,
+    # metric_for_best_model='pcl_f1'
+)
 
 trainer = CustomTrainer(
-        model=model,                         
-        args=training_args,                 
-        train_dataset=train_dataset,                   
-        data_collator=eval_dataset.collate_fn,
-        compute_metrics = compute_metric_eval,
-        eval_dataset = eval_dataset
-    )
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    data_collator=eval_dataset.collate_fn,
+    compute_metrics=compute_metric_eval,
+    eval_dataset=eval_dataset
+)
 trainer.train()
 
 trainer.save_model(model_path)
@@ -164,7 +164,7 @@ val_df.to_pickle('val_df.pickle')
 
 config = AutoConfig.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-model = AutoModelForSequenceClassification.from_pretrained(model_path , config = config).to(device)
+model = AutoModelForSequenceClassification.from_pretrained(model_path, config=config).to(device)
 
 train_df = pd.read_pickle('train_df.pickle')
 val_df = pd.read_pickle('val_df.pickle')
@@ -172,51 +172,66 @@ val_df = pd.read_pickle('val_df.pickle')
 train_dataset = PCLDataset(tokenizer, train_df)
 eval_dataset = PCLDataset(tokenizer, val_df)
 
-def predict_pcl(input, tokenizer, model): 
-  model.eval()
-  encodings = tokenizer(input, return_tensors='pt', padding=True, truncation=True, max_length=256)
-  encodings = encodings.to(device)
-  output = model(**encodings)
-  logits = output.logits
-  preds = torch.max(logits, 1)
 
-  return {'prediction':preds[1], 'confidence':preds[0]}
+def predict_pcl(input, tokenizer, model, threshold=0.5):
+    model.eval()
+    encodings = tokenizer(input, return_tensors='pt', padding=True, truncation=True, max_length=256)
+    encodings = encodings.to(device)
+    output = model(**encodings)
+    logits = output.logits
+    preds = np.zeros(logits.shape)
+    preds[preds > threshold] = 1
 
-def evaluate(model, tokenizer, data_loader):
+    return {'prediction': preds, 'confidence': logits[:, 1]}
 
-  preds = []
-  tot_labels = []
 
-  with torch.no_grad():
-    for data in (data_loader): 
+def evaluate(model, tokenizer, data_loader, threshold=0.5):
+    preds = []
+    tot_labels = []
+    confidences = []
+    with torch.no_grad():
+        for data in (data_loader):
+            labels = {}
+            labels['label'] = data['label']
 
-      labels = {}
-      labels['label'] = data['label']
+            tweets = data['text']
 
-      tweets = data['text']
+            pred = predict_pcl(tweets, tokenizer, model, threshold)
 
-      pred = predict_pcl(tweets, tokenizer, model)
+            preds.append(np.array(pred['prediction'].cpu()))
+            tot_labels.append(np.array(labels['label'].cpu()))
+            confidences.append(np.array(pred['confidence'].cpu()))
 
-      preds.append(np.array(pred['prediction'].cpu()))
-      tot_labels.append(np.array(labels['label'].cpu()))
+    # with the saved predictions and labels we can compute accuracy, precision, recall and f1-score
 
-  # with the saved predictions and labels we can compute accuracy, precision, recall and f1-score
-  
+    return preds, tot_labels, confidences
 
-  return preds, tot_labels
 
 validation_loader = DataLoader(eval_dataset)
 
-preds, tot_labels = evaluate(model, tokenizer, validation_loader)
+preds, tot_labels, confidences = evaluate(model, tokenizer, validation_loader)
 tot_labels = np.array(tot_labels)
 preds = np.array(preds)
-report = classification_report(tot_labels, preds, target_names=["Not PCL","PCL"], output_dict= True)
+report = classification_report(tot_labels, preds, target_names=["Not PCL", "PCL"], output_dict=True)
 print(report)
 
 print(report['accuracy'])
 print(report['Not PCL']['f1-score'])
 print(report['PCL']['f1-score'])
 
+# define threshold
+pcl_count_by_threshold = []
+non_pcl_count_by_threshold = []
+f1_by_threshold = []
+for percentage in range(100):
+    threshold = percentage / 100
+    pcl_count = (confidences > threshold).sum()
+    non_pcl_count = (confidences <= threshold).sum()
+    pred = np.zeros(tot_labels.shape)
+    pred[confidences > threshold] = 1
+    f1_by_threshold.append(f1_score(tot_labels, pred, labels=['Non PCL', 'PCL'])['PCL'])
+
+best_threshold = np.argmax(f1_by_threshold) / 100
 """# Test set"""
 
 dpm_pp.load_test()
@@ -226,7 +241,7 @@ test_dataset = PCLDataset(tokenizer, test_df)
 
 test_loader = DataLoader(test_dataset)
 
-preds, tot_labels = evaluate(model, tokenizer, test_loader)
+preds, tot_labels, confidences = evaluate(model, tokenizer, test_loader, best_threshold)
 tot_labels = np.array(tot_labels)
 preds = np.array(preds)
 # report = classification_report(tot_labels, preds, target_names=["Not PCL","PCL"], output_dict= True)
@@ -240,16 +255,18 @@ preds = np.array(preds)
 preds.shape
 
 from collections import Counter
+
 preds = preds.reshape(-1)
 Counter(preds)
 
+
 # helper function to save predictions to an output file
 def labels2file(p, outf_path):
-	with open(outf_path,'w') as outf:
-		for pi in p:
-			outf.write(','.join([str(k) for k in pi])+'\n')
+    with open(outf_path, 'w') as outf:
+        for pi in p:
+            outf.write(','.join([str(k) for k in pi]) + '\n')
+
 
 labels2file([[k] for k in preds], 'task1.txt')
 os.system("!cat task1.txt | head -n 10")
 os.system("!zip submission.zip task1.txt")
-
